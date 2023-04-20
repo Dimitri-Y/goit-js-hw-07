db076148d0d6ca32326753deeb59e097e2076391
Dimitri-Y <dimoon.bond@gmail.com>
2023-04-20 10:23:38 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -8,32 +8,33 @@ const modalImg=(event)=>{
     <img src=${event.target.dataset.source} width="800" height="600">
 `);
     instance.show();
-    document.addEventListener("keydown", (event) => onEscPress(event, instance));
 };
 
-const insertHtmlImg= ()=>{
-    galleryItems.forEach(item=>{
-        const text = `<li class="gallery__item">\n
-        <a class="gallery__link" href=${item.original}>\n
-        <img class="gallery__image" 
-        src=${item.preview} 
-        data-source=${item.original} 
-        alt=${item.description}/>
-        </a>
-        </li>`;
-        textHtml+=text;
-    });
+// const insertHtmlImg= ()=>{
+//     galleryItems.forEach(item=>{
+//         const textHtml += `<li class="gallery__item">\n
+//         <a class="gallery__link">\n
+//         <img class="gallery__image" 
+//         src=${item.preview} 
+//         data-source=${item.original} 
+//         alt=${item.description}/>
+//         </a>
+//         </li>`;
+//     });
+//     ulSelector.insertAdjacentHTML("afterbegin",textHtml);
+// };
+
+const insertHtmlImg=()=>{
+    textHtml=galleryItems.map((item) => `<li class="gallery__item">
+             <a class="gallery__link">
+             <img class="gallery__image" 
+             src=${item.preview} 
+             data-source=${item.original} 
+             alt=${item.description}/>
+             </a>
+             </li>`).join("\n");
     ulSelector.insertAdjacentHTML("afterbegin",textHtml);
 };
 
-const onEscPress = (event, instance) => {
-    const ESC_KEYCODE = "Escape";
-    if (event.code === ESC_KEYCODE) {
-      instance.close();
-      document.removeEventListener("keydown", (event) =>
-        onEscPress(event, instance)
-      );
-    }
-};
 insertHtmlImg();
 ulSelector.addEventListener("click",(event)=>modalImg(event));

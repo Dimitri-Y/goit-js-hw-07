42340fac97484dedba040ee946084e44d0fb8cdc
Dimitri-Y <dimoon.bond@gmail.com>
2023-04-21 23:15:58 +0300

    update lightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,23 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 
-let textHtml='';
-const ulSelector =document.querySelector("ul.gallery");
+let textHtml = "";
+const ulSelector = document.querySelector("ul.gallery");
 let escAttribute = false;
-const modalImg=(event)=>{
-    const instance = basicLightbox.create(`
+const modalImg = (event) => {
+  const instance = basicLightbox.create(`
     <img src=${event.target.dataset.source} width="800" height="600">
 `);
-    instance.show();
-    document.addEventListener("keydown", (event) => onEscPress(event, instance));
+  instance.show();
+  document.addEventListener("keydown", (event) => onEscPress(event, instance));
 };
 
 // const insertHtmlImg= ()=>{
 //     galleryItems.forEach(item=>{
 //         const textHtml += `<li class="gallery__item">\n
 //         <a class="gallery__link">\n
-//         <img class="gallery__image" 
-//         src=${item.preview} 
-//         data-source=${item.original} 
+//         <img class="gallery__image"
+//         src=${item.preview}
+//         data-source=${item.original}
 //         alt=${item.description}/>
 //         </a>
 //         </li>`;
@@ -25,24 +25,29 @@ const modalImg=(event)=>{
 //     ulSelector.insertAdjacentHTML("afterbegin",textHtml);
 // };
 
-const insertHtmlImg=()=>{
-    textHtml=galleryItems.map((item) => `<li class="gallery__item">
+const insertHtmlImg = () => {
+  textHtml = galleryItems
+    .map(
+      (item) => `<li class="gallery__item">
              <a class="gallery__link">
              <img class="gallery__image" 
              src=${item.preview} 
              data-source=${item.original} 
              alt=${item.description}/>
              </a>
-             </li>`).join("\n");
-    ulSelector.insertAdjacentHTML("afterbegin",textHtml);
+             </li>`
+    )
+    .join("\n");
+  ulSelector.insertAdjacentHTML("afterbegin", textHtml);
 };
 const onEscPress = (event, instance) => {
-    const ESC_KEYCODE = "Escape";
-    if (event.code === ESC_KEYCODE) {
-      instance.close();
-      document.removeEventListener("keydown", (event) =>
-        onEscPress(event, instance)
-      ); }
-  };  
+  const ESC_KEYCODE = "Escape";
+  if (event.code === ESC_KEYCODE) {
+    instance.close();
+    document.removeEventListener("keydown", (event) =>
+      onEscPress(event, instance)
+    );
+  }
+};
 insertHtmlImg();
-ulSelector.addEventListener("click",(event)=>modalImg(event));
+ulSelector.addEventListener("click", (event) => modalImg(event));

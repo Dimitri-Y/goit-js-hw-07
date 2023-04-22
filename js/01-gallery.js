79043f4120e05c16bb98639b516cb4d6cbb04253
Dimitri-Y <dimoon.bond@gmail.com>
2023-04-22 23:11:34 +0300

    update js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,27 +3,32 @@ import { galleryItems } from "./gallery-items.js";
 let textHtml = "";
 const ulSelector = document.querySelector("ul.gallery");
 let escAttribute = false;
+
 const modalImg = (event) => {
-  const instance = basicLightbox.create(`
-    <img src=${event.target.dataset.source} width="800" height="600">
-`);
+  const instance = basicLightbox.create(
+    `
+  <img src=${event.target.dataset.source} width="800" height="600">
+`,
+    {
+      onShow: (instance) =>
+        document.addEventListener("keydown", (event) =>
+          onEscPress(event, instance)
+        ),
+      onClose: (instance) =>
+        document.removeEventListener("keydown", (event) =>
+          onEscPress(event, instance)
+        ),
+    }
+  );
   instance.show();
-  document.addEventListener("keydown", (event) => onEscPress(event, instance));
 };
 
-// const insertHtmlImg= ()=>{
-//     galleryItems.forEach(item=>{
-//         const textHtml += `<li class="gallery__item">\n
-//         <a class="gallery__link">\n
-//         <img class="gallery__image"
-//         src=${item.preview}
-//         data-source=${item.original}
-//         alt=${item.description}/>
-//         </a>
-//         </li>`;
-//     });
-//     ulSelector.insertAdjacentHTML("afterbegin",textHtml);
-// };
+const onEscPress = (event, instance) => {
+  const ESC_KEYCODE = "Escape";
+  if (event.code === ESC_KEYCODE) {
+    instance.close();
+  }
+};
 
 const insertHtmlImg = () => {
   textHtml = galleryItems
@@ -40,14 +45,20 @@ const insertHtmlImg = () => {
     .join("\n");
   ulSelector.insertAdjacentHTML("afterbegin", textHtml);
 };
-const onEscPress = (event, instance) => {
-  const ESC_KEYCODE = "Escape";
-  if (event.code === ESC_KEYCODE) {
-    instance.close();
-    document.removeEventListener("keydown", (event) =>
-      onEscPress(event, instance)
-    );
-  }
-};
+
+// const insertHtmlImg= ()=>{
+//     galleryItems.forEach(item=>{
+//         const textHtml += `<li class="gallery__item">\n
+//         <a class="gallery__link">\n
+//         <img class="gallery__image"
+//         src=${item.preview}
+//         data-source=${item.original}
+//         alt=${item.description}/>
+//         </a>
+//         </li>`;
+//     });
+//     ulSelector.insertAdjacentHTML("afterbegin",textHtml);
+// };
+
 insertHtmlImg();
 ulSelector.addEventListener("click", (event) => modalImg(event));
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,45 +3,45 @@ import { galleryItems } from "./gallery-items.js";
 const galleryList = document.querySelector("ul.gallery");
 let newList = [];
 
-galleryItems.forEach(({ preview, original, description }) => {
+newList = galleryItems.map((item) => {
   const galleryItem = document.createElement("li");
   galleryItem.classList.add("gallery__item");
 
   const link = document.createElement("a");
   link.classList.add("gallery__link");
-  link.href = original;
+  link.href = item.original;
 
   const image = document.createElement("img");
   image.classList.add("gallery__image");
-  image.src = preview;
-  image.alt = description;
+  image.src = item.preview;
+  image.alt = item.description;
 
   link.append(image);
   galleryItem.append(link);
-  galleryList.append(galleryItem);
+  return galleryItem;
 });
+
+galleryList.append(...newList);
 
 const lightbox = new SimpleLightbox(".gallery a", {
   captionsData: "alt",
   captionDelay: 250,
 });
 
-// galleryItems.forEach((item) => {
+// galleryItems.forEach(({ preview, original, description }) => {
 //   const galleryItem = document.createElement("li");
 //   galleryItem.classList.add("gallery__item");
 
 //   const link = document.createElement("a");
 //   link.classList.add("gallery__link");
-//   link.href = item.original;
+//   link.href = original;
 
 //   const image = document.createElement("img");
 //   image.classList.add("gallery__image");
-//   image.src = item.preview;
-//   image.alt = item.description;
+//   image.src = preview;
+//   image.alt = description;
 
 //   link.append(image);
 //   galleryItem.append(link);
-//   newList.push(galleryItem);
+//   galleryList.append(galleryItem);
 // });
-
-// galleryList.append(...newList);
